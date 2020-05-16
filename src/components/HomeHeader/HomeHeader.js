235c966e56a9b48cdbc1e65e379d10092dc3d559
jaomaloy <jaomaloy@gmail.com>
2020-05-16 18:12:51 +0800

    Added components to the HomePage: The Header and the info cards

---
+++ src/components/HomeHeader/HomeHeader.js
@@ -0,0 +1,27 @@
+import React from 'react'; 
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import title from './app-title.png';
+
+const useStyles = makeStyles({
+  jumbotron: {
+    color: 'white',
+    paddingBottom: '35px'
+  },
+  typography: {
+    fontWeight: 'lighter'
+  },
+});
+
+const HomeHeader = () => {
+  const classes = useStyles();
+
+ return (
+    <div className={classes.jumbotron}>
+        <img alt='logo' src={title}/>
+        <Typography className={classes.typography} variant='h5' gutterBottom> {'Easy ways to compare your images'} </Typography>
+    </div>
+  );
+}
+
+export default HomeHeader;

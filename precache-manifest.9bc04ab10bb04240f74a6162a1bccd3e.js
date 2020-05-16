f2d2f8b76583e1d8339c58041741f1193f4ea8d6
jaomaloy <jaomaloy@gmail.com>
2020-05-16 20:22:38 +0800

    Updates

---
+++ precache-manifest.9bc04ab10bb04240f74a6162a1bccd3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c6d5ef3b0645bde75bbf66a90407ef",
+    "url": "/ccomparator/index.html"
+  },
+  {
+    "revision": "2204893a29ab716766a9",
+    "url": "/ccomparator/static/css/2.7063d15d.chunk.css"
+  },
+  {
+    "revision": "b8ec4c4653bc612bf158",
+    "url": "/ccomparator/static/css/main.f7207364.chunk.css"
+  },
+  {
+    "revision": "2204893a29ab716766a9",
+    "url": "/ccomparator/static/js/2.fdc8774b.chunk.js"
+  },
+  {
+    "revision": "c80e979080b9b5180c4b83d8e6cb7127",
+    "url": "/ccomparator/static/js/2.fdc8774b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8ec4c4653bc612bf158",
+    "url": "/ccomparator/static/js/main.4ead8a0a.chunk.js"
+  },
+  {
+    "revision": "458efba25b515c835fed",
+    "url": "/ccomparator/static/js/runtime-main.4e0f269c.js"
+  },
+  {
+    "revision": "40f0e89cb9d6383f956bcbf73fcbc4f8",
+    "url": "/ccomparator/static/media/1v1.40f0e89c.png"
+  },
+  {
+    "revision": "dfe805b76476e5471cfec93b9ad18025",
+    "url": "/ccomparator/static/media/casual-mode.dfe805b7.png"
+  }
+]);
